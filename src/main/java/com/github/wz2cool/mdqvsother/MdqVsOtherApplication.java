fec5358e9623995fc302a2d98c025fbce9898a36
wz2cool <wz2cool@live.cn>
2019-06-14 11:46:10 +0800

    add

---
+++ src/main/java/com/github/wz2cool/mdqvsother/MdqVsOtherApplication.java
@@ -0,0 +1,13 @@
+package com.github.wz2cool.mdqvsother;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdqVsOtherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MdqVsOtherApplication.class, args);
+    }
+
+}
